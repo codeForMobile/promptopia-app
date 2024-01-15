54db43905e9a00533453d31321ea7f2100327025
codeForMobile <code.mobile@protonmail.com>
2024-01-14 19:20:15 -0600

    add update and delete post route

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -5,7 +5,16 @@ import { useSession } from 'next-auth/react'
 import { usePathname, useRouter} from 'next/navigation'
 
 const PromptCard = ({post, handleTagClick, handleEdit, handleDelete}) => {
+  const { data: session } = useSession()
+  const pathname = usePathname()
+  const router = useRouter()
   const [copy, setCopy]= useState('')
+
+  const handleCopy = () => {
+    setCopy(post.prompt)
+    navigator.clipboard.writeText(post.prompt)
+    setTimeout(() => setCopy(''), 3000);
+  }
   return (
     <div className='prompt_card'>
       <div className='flex justify-between image-start gap-5'>
@@ -26,7 +35,7 @@ const PromptCard = ({post, handleTagClick, handleEdit, handleDelete}) => {
           </div>
         </div>
 
-        <div className='copy_btn' onClick={() => {}}>
+        <div className='copy_btn' onClick={handleCopy}>
           <Image 
           src={copy === post.promot
             ? '/assets/icons/tick.svg'
@@ -43,8 +52,22 @@ const PromptCard = ({post, handleTagClick, handleEdit, handleDelete}) => {
       <p className='font-inter text-sm blue_gradient cursor-pointer'
         onClick={() => handleTagClick && handleTagClick(post.tag)}
       >
-        {post.tag}
+        #{post.tag}
       </p>
+
+      {session?.user.id === post.creator._id && 
+      pathname === '/profile' && (
+        <div className='mt-5 flex-center gap-4 border-t border-gray-100 pt-3'>
+          <p className='font-inter text-sm green_gradient cursor-pointer'
+            onClick={handleEdit}
+          >Edit
+          </p>
+          <p className='font-inter text-sm orange_gradient cursor-pointer'
+            onClick={handleDelete}
+          >Delete
+          </p>
+        </div>
+      )}
     </div>
   )
 }
